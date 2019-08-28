ff3104e0013afe2de24e12a8601959006c198bcb
Your Name <potawsky.yuri@gmail.com>
2019-08-27 21:50:15 -0400

    newest

---
+++ scripts.js
@@ -0,0 +1,26 @@
+//window.prompt("hello");
+
+let Bike = {
+    brand: "Ragley",
+    color: "gunmetal",
+    travel: [160, 150],
+    createBike: (A, B)=>{
+        console.log(A, B);
+    }
+};
+
+const Hibriten = (height)=>{
+    this.height = height;
+}
+
+
+
+//let test = document.URL;
+//console.log(test);
+
+Bike.createBike("Ragley", "Piglet");
+console.log(Bike["brand"]);
+//window.alert(Bike.travel);
+
+
+
